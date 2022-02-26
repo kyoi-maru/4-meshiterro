b60e542871f04b59d3702638283ce06655b3ceb7
kyoi-maru <99070966+kyoi-maru@users.noreply.github.com>
2022-02-26 10:02:58 +0000

    [Finish]4-20

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -11,6 +11,7 @@ class PostCommentsController < ApplicationController
   def destroy
     PostComment.find(params[:id]).destroy
     redirect_to post_image_path(params[:post_image_id])
+  end
 
   private
 
